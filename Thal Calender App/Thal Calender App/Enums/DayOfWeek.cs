94116eb960b95cbc6235dfc3ebd97c936eea20ee
Vendare <Marco.Griesel@gmx.de>
2016-12-02 09:45:15 +0100

    DateTime Objekt angelegt Enum für Tage angelegt

---
+++ Thal Calender App/Thal Calender App/Enums/DayOfWeek.cs
@@ -0,0 +1,13 @@
+﻿namespace Thal_Calender_App.Enums
+{
+    enum DayOfWeek
+    {
+        Samstag    = 0,
+        Sonntag    = 1,
+        Montag     = 2,
+        Dienstag   = 3,
+        Mittwoch   = 4,
+        Donnerstag = 5,
+        Freitag    = 6
+    }
+}
